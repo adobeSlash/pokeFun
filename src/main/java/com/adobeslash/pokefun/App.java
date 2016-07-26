3e0bee34ee7686a96cb6bab013dc96d66a74e5c9
stephane <stephane.karagulmez@etu.utc>
2016-07-26 09:04:46 +0200

    delete auto-retry

--- src/main/java/com/adobeslash/pokefun/App.java
+++ src/main/java/com/adobeslash/pokefun/App.java
@@ -29,10 +29,11 @@ public class App
     	System.out.println("Hello");
     	OkHttpClient httpClient = new OkHttpClient(); 
     	SendMailTLS mail= new SendMailTLS();
-    	String token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBiZDEwY2JmMDM2OGQ2MWE0NDBiZjYxZjNiM2EyZDI0NGExODQ5NDcifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoidVNwWUVxMEhGWDFPMWJCbTg0cWJYZyIsImF1ZCI6Ijg0ODIzMjUxMTI0MC03M3JpM3Q3cGx2azk2cGo0Zjg1dWo4b3RkYXQyYWxlbS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwODA3OTQ1ODU2NTY4NDkyMzg3NSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI4NDgyMzI1MTEyNDAtNzNyaTN0N3Bsdms5NnBqNGY4NXVqOG90ZGF0MmFsZW0uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6InNsYXNodHV0b3JpZWxAZ21haWwuY29tIiwiaWF0IjoxNDY5NTEwMDM5LCJleHAiOjE0Njk1MTM2Mzl9.Et5UvHejGvadenq2DQij3dARozKMMJzh4U3dPMMtZz4bRa6jX17lp4Ecn6Jiu_41RdjlH7cgM9HgDR-QYO9Lp8MZxdCHXzEUO_n-skD_T6Z_PDfW9riyu7iwAi8z_-323Lr5cZPuj6zWdvhN10SIGYB-pztU9OV4qozFxzB1JL_hrHlW-eLss8RceTkvmnzVBf1DGcrY5PCwmZWMTlKFsWpEcY9KjlFzZSpA8r_rC_Pz0KbfsrGpemKrUBNMsMOgZ4Ez5rmO_zho2eFO9nOL_k117yR1tHZaCcro73FhX18GDdz-gHsOjGfTiH_41woNKEQQRZsPuTPEbVBj1Th-xg";
-    	
+    	String token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBiZDEwY2JmMDM2OGQ2MWE0NDBiZjYxZjNiM2EyZDI0NGExODQ5NDcifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdF9oYXNoIjoiUWhseVlGQlRBcW0xNThUX2tjSWRTUSIsImF1ZCI6Ijg0ODIzMjUxMTI0MC03M3JpM3Q3cGx2azk2cGo0Zjg1dWo4b3RkYXQyYWxlbS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwODA3OTQ1ODU2NTY4NDkyMzg3NSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhenAiOiI4NDgyMzI1MTEyNDAtNzNyaTN0N3Bsdms5NnBqNGY4NXVqOG90ZGF0MmFsZW0uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJlbWFpbCI6InNsYXNodHV0b3JpZWxAZ21haWwuY29tIiwiaWF0IjoxNDY5NTE0NDk1LCJleHAiOjE0Njk1MTgwOTV9.P4QPor8Vd4ZY9aeiSsSYOwnwFVzvgvwD6TqyO_wOlF5X4t8VUuFwCg_l66FXpMnah2juhe0wQIVnTbJTQZzmU2xizBZlM6InEYeyxyYWbtM1PmPXR0cbaoMyWrwIMhq-ngojv2d6BYlXNc-7EX0bLOZEsy9g1LMsB0zHj_fevZmM6xLZZZRu_NiRi-h4J2ETJnQPlLtFNhcoU0q7uyV9SpPfmU6g5CwUOrCpAqpjgqeM_cLwKlqjVcYk8tllmRq2m4_2rCZyFRP8--ME_3YQuZq5F1GJWTaKLcpVxFWaP4EGqEGPHn7uHHesK130sXmHEMbck9Y09mhBvHPcowRKDQ";
+ 	
 		try {
 			AuthInfo auth = new GoogleLogin(httpClient).login(token); 
+			//AuthInfo auth = new GoogleLogin(httpClient).login("slashtutoriel@gmil.com","c@stor__88"); 
 			PokemonGo go = new PokemonGo(auth,httpClient);
 			//go.setLocation( 48.8086335, 2.1335094999999455, 0); //Maison
 			go.setLocation(48.80962619260876, 2.134148, 0); //Gare RD
@@ -43,7 +44,7 @@ public class App
 					+ go.getAltitude() + "-");
 			System.out.println("profile : " + go.getPlayerProfile().getUsername());
 			while(true){
-				Thread.sleep(120000);
+				Thread.sleep(5000);
 				System.out.println("scanned");
 				ArrayList <CatchablePokemon> catchables = (ArrayList<CatchablePokemon>) go.getMap().getCatchablePokemon();
 				for(CatchablePokemon pokemon : catchables){
@@ -85,8 +86,8 @@ public class App
 				}
 			}
 		} catch (LoginFailedException e) {
+			e.printStackTrace();
 			mail.sendMailLoginException(e);
-			main(null);
 		} catch (RemoteServerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
