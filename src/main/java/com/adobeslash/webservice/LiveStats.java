08c28831b5474921f006872aa04169556bedb3b7
stephane <stephane.karagulmez@etu.utc>
2016-07-27 19:47:29 +0200

    object use to get live stats in the futur.. juste started

---
+++ src/main/java/com/adobeslash/webservice/LiveStats.java
@@ -0,0 +1,83 @@
+package com.adobeslash.webservice;
+
+import com.pokegoapi.api.PokemonGo;
+import com.pokegoapi.exceptions.LoginFailedException;
+import com.pokegoapi.exceptions.RemoteServerException;
+
+import POGOProtos.Inventory.Item.ItemIdOuterClass.ItemId;
+
+/**
+ * 
+ * @author adobe
+ * Class use to send live stats through web Service
+ */
+public class LiveStats {
+	
+	private final String username;
+	private int level;
+	private int itemStorage;
+	private int pokeball;
+	private int superball;
+	private int hyperball;
+	
+
+	public LiveStats(final PokemonGo go) throws LoginFailedException, RemoteServerException {
+		this.username = go.getPlayerProfile().getUsername();
+		updateStats(go);
+	}
+	
+	public void updateStats(final PokemonGo go) throws LoginFailedException, RemoteServerException{
+		go.getInventories().updateInventories();
+		this.level = go.getPlayerProfile().getStats().getLevel();
+		this.itemStorage = go.getPlayerProfile().getItemStorage();
+		this.pokeball = go.getInventories().getItemBag().getItem(ItemId.ITEM_POKE_BALL).getCount();
+		this.superball = go.getInventories().getItemBag().getItem(ItemId.ITEM_GREAT_BALL).getCount();
+		this.hyperball = go.getInventories().getItemBag().getItem(ItemId.ITEM_ULTRA_BALL).getCount();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public int getItemStorage() {
+		return itemStorage;
+	}
+
+	public void setItemStorage(int itemStorage) {
+		this.itemStorage = itemStorage;
+	}
+
+	public int getPokeball() {
+		return pokeball;
+	}
+
+	public void setPokeball(int pokeball) {
+		this.pokeball = pokeball;
+	}
+
+	public int getSuperball() {
+		return superball;
+	}
+
+	public void setSuperball(int superball) {
+		this.superball = superball;
+	}
+
+	public int getHyperball() {
+		return hyperball;
+	}
+
+	public void setHyperball(int hyperball) {
+		this.hyperball = hyperball;
+	}
+	
+
+}
