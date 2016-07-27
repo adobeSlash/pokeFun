20107bf329399d501c22d0e8186462a14904e089
Courtin Charles-Eric <charles-eric_courtin@ens.univ-artois.fr>
2016-07-27 22:54:05 +0200

    Mode itineraire activé

--- src/main/java/com/adobeslash/pokefun/App.java
+++ src/main/java/com/adobeslash/pokefun/App.java
@@ -1,12 +1,15 @@
 package com.adobeslash.pokefun;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
-import org.apache.log4j.Logger;
+import javax.xml.parsers.ParserConfigurationException;
 
-import POGOProtos.Networking.Responses.CatchPokemonResponseOuterClass.CatchPokemonResponse.CatchStatus;
+import org.apache.log4j.Logger;
+import org.xml.sax.SAXException;
 
 import com.adobeslash.listener.OnGoogleAuthListener;
+import com.adobeslash.pokeutils.KmlParser;
 import com.adobeslash.pokeutils.PokeHelper;
 import com.adobeslash.pokeutils.PokeMove;
 import com.adobeslash.pokeutils.PokeStats;
@@ -18,76 +21,79 @@ import com.pokegoapi.auth.GoogleCredentialProvider;
 import com.pokegoapi.exceptions.LoginFailedException;
 import com.pokegoapi.exceptions.RemoteServerException;
 
+import POGOProtos.Networking.Responses.CatchPokemonResponseOuterClass.CatchPokemonResponse.CatchStatus;
 import okhttp3.OkHttpClient;
 
-public class App 
-{
+public class App {
 	final static Logger logger = Logger.getLogger(App.class);
-	
-    public static void main( String[] args ) throws InterruptedException
-    {
-    	
-    	OkHttpClient httpClient = new OkHttpClient(); 
-    	OnGoogleAuthListener authListener;
-    	
-    	//Proxy worldline...
-//		OkHttpClient httpClient = new OkHttpClient.Builder()
-//		    .connectTimeout(60, TimeUnit.SECONDS)
-//		    .writeTimeout(60, TimeUnit.SECONDS)
-//		    .readTimeout(60, TimeUnit.SECONDS)
-//		    .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("prx-dev02", 3128))).build();
-    		
+
+	public static void main(String[] args)
+			throws InterruptedException, ParserConfigurationException, SAXException, IOException {
+
+		OkHttpClient httpClient = new OkHttpClient();
+		OnGoogleAuthListener authListener;
+
+		// Proxy worldline...
+		// OkHttpClient httpClient = new OkHttpClient.Builder()
+		// .connectTimeout(60, TimeUnit.SECONDS)
+		// .writeTimeout(60, TimeUnit.SECONDS)
+		// .readTimeout(60, TimeUnit.SECONDS)
+		// .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("prx-dev02",
+		// 3128))).build();
+
 		try {
-			authListener = new OnGoogleAuthListener();		
-			PokemonGo go = new PokemonGo(new GoogleCredentialProvider(httpClient, authListener),httpClient);
+			authListener = new OnGoogleAuthListener();
+			PokemonGo go = new PokemonGo(new GoogleCredentialProvider(httpClient, authListener), httpClient);
 			PokeStats tracer = new PokeStats(go);
-			
-			//go.setLocation( 48.8086335, 2.1335094999999455, 0); //Maison
-			//go.setLocation(48.80962619260876, 2.134148, 0); //Gare RD
-			go.setLocation(48.8615963, 2.289282299999968, 0); // Parc du trocadero
-			//go.setLocation(48.856181844312594, 2.2977787494903623, 0); // Eiffel
-			//go.setLocation(48.863492, 2.327494, 0); // Jardin des Tuileries
-			//go.setLocation(48.892416, 2.393335, 0); // La vilette
-			logger.info("location : " + go.getLatitude() + "-"
-					+ go.getLongitude() + "-"
-					+ go.getAltitude() + "-");
+			String itineraire = "src/main/resources/itineraireTest.xml";
+			// go.setLocation( 48.8086335, 2.1335094999999455, 0); //Maison
+			// go.setLocation(48.80962619260876, 2.134148, 0); //Gare RD
+			double[] start = new KmlParser().getCoordinatesFromKml(itineraire).get(0);
+			go.setLocation(start[1], start[0], 0);
+			// go.setLocation(48.8615963, 2.289282299999968, 0); // Parc du
+			// trocadero
+			// go.setLocation(48.856181844312594, 2.2977787494903623, 0); Eiffel
+			// go.setLocation(48.863492, 2.327494, 0); // Jardin des Tuileries
+			// go.setLocation(48.892416, 2.393335, 0); // La vilette
+			logger.info("location : " + go.getLatitude() + "-" + go.getLongitude() + "-" + go.getAltitude() + "-");
 			logger.info("profile : " + go.getPlayerProfile().getUsername());
-			
-			PokeMove pm = new PokeMove(go);
+
+			PokeMove pm = new PokeMove(go, itineraire);
 			pm.start();
-			
-			while(true){
-				
+
+			while (true) {
+
 				Thread.sleep(10000);
 				logger.info("start scan...");
-				
-				ArrayList <CatchablePokemon> catchables = (ArrayList<CatchablePokemon>) go.getMap().getCatchablePokemon();
-				for(CatchablePokemon pokemon : catchables){
+
+				ArrayList<CatchablePokemon> catchables = (ArrayList<CatchablePokemon>) go.getMap()
+						.getCatchablePokemon();
+				for (CatchablePokemon pokemon : catchables) {
 					logger.info("catchables : " + pokemon.getPokemonId());
-					
+
 					EncounterResult encResult = pokemon.encounterPokemon();
-					
+
 					if (encResult.wasSuccessful()) {
 						logger.info("Encounted:" + pokemon.getPokemonId());
 						CatchResult result = pokemon.catchPokemon();
 						logger.info("Attempt to catch:" + pokemon.getPokemonId() + " " + result.getStatus());
-						
-						if(result.getStatus().equals(CatchStatus.CATCH_SUCCESS)){
+
+						if (result.getStatus().equals(CatchStatus.CATCH_SUCCESS)) {
 							PokeHelper.askForTransfer(go, pokemon, tracer);
 						}
-					}else{
+					} else {
 						logger.info("failed to catch pokemon");
 					}
 				}
-				
-				//PokeHelper.lootNearestPokestop(go);	
+
+				PokeHelper.lootNearestPokestop(go);
 				logger.info("end of scan...");
 			}
 		} catch (LoginFailedException e) {
 			logger.error(e.getLocalizedMessage());
-			
+
 		} catch (RemoteServerException e) {
 			logger.error(e.getLocalizedMessage());
 		}
-    }
+	}
 }
--- src/main/java/com/adobeslash/pokeutils/PokeMove.java
+++ src/main/java/com/adobeslash/pokeutils/PokeMove.java
@@ -15,109 +15,162 @@ import com.pokegoapi.exceptions.LoginFailedException;
 import com.pokegoapi.exceptions.RemoteServerException;
 
 public class PokeMove extends Thread {
-  final static Logger logger = Logger.getLogger(PokeMove.class);
-  public PokemonGo go;
-  
-  public PokeMove(PokemonGo go){
-	  this.go = go;
-  }
+	final static Logger logger = Logger.getLogger(PokeMove.class);
+	public PokemonGo go;
+	private String itineraire = null;
 
-  public void run() {
-    while (true) {
-      try {
-    	ArrayList<Pokestop> lesPokestops = new ArrayList<Pokestop>((ArrayList<Pokestop>) go.getMap().getMapObjects().getPokestops());
-        //Longitude,Latitude
-    	List<double[]> itineraire = KmlParser.getCoordinatesFromKml("src/main/resources/itineraireTest.xml");
-        	
-        Pokestop nearest = null;
-        Double distance = null;
-        for (Pokestop p : lesPokestops) {
-          if (p.canLoot()) {
-            p.loot();
-            logger.info("Looter le pokestop " + p.getDetails().getName());
-          } else {
-            if (p.canLoot(true)) {
-              double distanceTmp = distance(lesPokestops.get(0).getLatitude(), lesPokestops.get(0).getLongitude(), go.getLatitude(), go.getLongitude());
-              if (nearest == null) {
-                nearest = p;
-                distance = distanceTmp;
-              } else {
-                distance = distanceTmp < distance ? distanceTmp : distance;
-                nearest = distanceTmp < distance ? p : nearest;
-              }
-            }
-          }
-        }
-        moveTo(nearest);
-
-      } catch (LoginFailedException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      } catch (RemoteServerException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      } catch (ParserConfigurationException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (SAXException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public PokeMove(PokemonGo go) {
+		this.go = go;
 	}
-    }
-  }
 
-  public double distance(double lat1, double lon1, double lat2, double lon2) {
-    double radians = Math.PI / 180;
-    double R = 6371; // km
-    double dLat = (lat2 - lat1) * radians;
-    double dLon = (lon2 - lon1) * radians;
-    lat1 = lat1 * radians;
-    lat2 = lat2 * radians;
+	public PokeMove(PokemonGo go, String itineraire) {
+		this.go = go;
+		this.itineraire = itineraire;
+	}
 
-    double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return R * c * 1000;// en metre sinon en km
-  }
+	public void run() {
+		if (itineraire == null)
+			runPokestop();
+		else
+			try {
+				runItineraire();
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
 
-  public void moveTo(Pokestop p) throws LoginFailedException, RemoteServerException {
-    // bouger 4 m par seconde pour que les pas soient compatibilisé
-	  logger.info("aller au pokestop  : " + p.getDetails().getName() + " : " + 
-    (int) distance(go.getLatitude(), p.getLongitude(), p.getLatitude(), p.getLongitude()) + " metres");
+	private void runItineraire() throws ParserConfigurationException, SAXException, IOException {
+		List<double[]> coordinates = new KmlParser().getCoordinatesFromKml(itineraire);
+		logger.info("nombre de point  : " + coordinates.size());
+		while (true) {
+			int i = 325;
+			for (; i < coordinates.size() - 1; i++) {
+				double[] nextPos = coordinates.get(i);
+				logger.info("aller au point suivant  : "
+						+ (int) distance(go.getLatitude(), go.getLongitude(), nextPos[1], nextPos[0]) + " metres index="
+						+ i);
 
-    double meters = 2;
-    double inRadian = (180 / Math.PI) * (meters / 6378137);
-    // longitude avant car se sert de la latitude
-    while (distance(p.getLatitude(), go.getLongitude(), p.getLatitude(), p.getLongitude()) > 12) {
+				moveTo(nextPos[0], nextPos[1]);
+			}
+			for (; i > 0; i--) {
+				double[] nextPos = coordinates.get(i);
+				logger.info("aller au point suivant  : "
+						+ (int) distance(go.getLatitude(), go.getLongitude(), nextPos[1], nextPos[0]) + " metres index="
+						+ i);
 
-      if (go.getLongitude() < p.getLongitude())
-        go.setLongitude(go.getLongitude() + inRadian / Math.cos(go.getLatitude()));
-      else
-        go.setLongitude(go.getLongitude() - inRadian / Math.cos(go.getLatitude()));
-      try {
-        Thread.sleep(1000l);
-      } catch (InterruptedException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-    }
-    logger.info("aller au pokestop  longitude OK: " + p.getDetails().getName());
-    while (distance(go.getLatitude(), p.getLongitude(), p.getLatitude(), p.getLongitude()) > 12) {
+				moveTo(nextPos[0], nextPos[1]);
+			}
+		}
 
-      if (go.getLatitude() < p.getLatitude())
-        go.setLatitude(go.getLatitude() + (180 / Math.PI) * (meters / 6378137));
-      else
-        go.setLatitude(go.getLatitude() - (180 / Math.PI) * (meters / 6378137));
-      try {
-        Thread.sleep(1000l);
-      } catch (InterruptedException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-    }
+	}
 
-  }
+	public void runPokestop() {
+		while (true) {
+			try {
+				ArrayList<Pokestop> lesPokestops = new ArrayList<Pokestop>(
+						(ArrayList<Pokestop>) go.getMap().getMapObjects().getPokestops());
+				// Longitude,Latitude
+				List<double[]> itineraire = KmlParser.getCoordinatesFromKml("src/main/resources/itineraireTest.xml");
+
+				Pokestop nearest = null;
+				Double distance = null;
+				for (Pokestop p : lesPokestops) {
+					if (p.canLoot()) {
+						p.loot();
+						logger.info("Looter le pokestop " + p.getDetails().getName());
+					} else {
+						if (p.canLoot(true)) {
+							double distanceTmp = distance(lesPokestops.get(0).getLatitude(),
+									lesPokestops.get(0).getLongitude(), go.getLatitude(), go.getLongitude());
+							if (nearest == null) {
+								nearest = p;
+								distance = distanceTmp;
+							} else {
+								distance = distanceTmp < distance ? distanceTmp : distance;
+								nearest = distanceTmp < distance ? p : nearest;
+							}
+						}
+					}
+				}
+				moveTo(nearest);
+
+			} catch (LoginFailedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (RemoteServerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public double distance(double lat1, double lon1, double lat2, double lon2) {
+		double radians = Math.PI / 180;
+		double R = 6371; // km
+		double dLat = (lat2 - lat1) * radians;
+		double dLon = (lon2 - lon1) * radians;
+		lat1 = lat1 * radians;
+		lat2 = lat2 * radians;
+
+		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+				+ Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		return R * c * 1000;// en metre sinon en km
+	}
+
+	public void moveTo(Pokestop p) throws LoginFailedException, RemoteServerException {
+		// bouger 4 m par seconde pour que les pas soient compatibilisé
+		logger.info("aller au pokestop  : " + p.getDetails().getName() + " : "
+				+ (int) distance(go.getLatitude(), p.getLongitude(), p.getLatitude(), p.getLongitude()) + " metres");
+		moveTo(p.getLongitude(), p.getLatitude());
+	}
+
+	public void moveTo(double lon, double lat) {
+		double meters = 6;
+		double inRadian = (180 / Math.PI) * (meters / 6378137);
+		// longitude avant car se sert de la latitude
+		while (distance(lat, go.getLongitude(), lat, lon) > 12) {
+
+			if (go.getLongitude() < lon)
+				go.setLongitude(go.getLongitude() + inRadian / Math.cos(go.getLatitude()));
+			else
+				go.setLongitude(go.getLongitude() - inRadian / Math.cos(go.getLatitude()));
+			try {
+				Thread.sleep(1000l);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		while (distance(go.getLatitude(), lon, lat, lon) > 12) {
+
+			if (go.getLatitude() < lat)
+				go.setLatitude(go.getLatitude() + (180 / Math.PI) * (meters / 6378137));
+			else
+				go.setLatitude(go.getLatitude() - (180 / Math.PI) * (meters / 6378137));
+			try {
+				Thread.sleep(1000l);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
 }
